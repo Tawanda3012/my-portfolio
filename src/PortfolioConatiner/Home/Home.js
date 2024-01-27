6423c1e4027cdd92f1c249fced8cdd02ad8230c7
Tawanda3012 <tawanda@uncommon.org>
2024-01-27 09:29:53 +0200

    new code

--- src/PortfolioConatiner/Home/Home.js
+++ src/PortfolioConatiner/Home/Home.js
@@ -1,34 +1,33 @@
 import React, { useRef } from "react";
+import { FaArrowDown } from "react-icons/fa";
+import { FaArrowUp } from "react-icons/fa";
 import "./Home.css";
 import Profile from "./Profile";
 import Sidebar from "../../components/Sidebar";
 import Footer from "./Footer/Footer";
 import Aboutme from "../AboutMe/Aboutme";
 import Skills from "../../components/Skills";
-import Projects from "../../components/Projects"
+import Projects from "../../components/Projects";
 
 function Home() {
   const pageRef = useRef(null);
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
 
-  const scrollToBottom = () => {
-    window.scrollTo({
-      top:10000,
-      behavior: "smooth",
-    });
-  };
+const scrollToBottom = () => {
+  window.scrollTo({ top: 10000, behavior: "smooth" });
+};
 
   return (
     <div className="home">
       <div className="home-container">
         <div ref={pageRef} className="first-button">
-          <button onClick={scrollToBottom}>Scroll to Bottom</button>
+          <button onClick={scrollToBottom}>
+             <FaArrowDown />{" "}
+            {/* Add the arrow down icon to the button */}
+          </button>
         </div>
         <Profile />
         <Sidebar />
@@ -36,10 +35,13 @@ function Home() {
       </div>
       <Aboutme />
       <Skills />
-      <Projects/>
+      <Projects />
       <Footer />
-      <div ref={pageRef} className="second-button" >
-        <button onClick={scrollToTop}>Scroll to Top</button>
+      <div ref={pageRef} className="second-button">
+      <button onClick={scrollToTop}>
+             <FaArrowUp />{" "}
+            {/* Add the arrow down icon to the button */}
+          </button>
       </div>
     </div>
   );
